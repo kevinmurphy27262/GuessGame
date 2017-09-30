7a7c48cebf36508036dc9d5c801a6425f4c1fb58
Kevin Murphy <kevin.murphy@hubinternational.com>
2017-09-30 13:45:35 -0500

    Began creating guessing game in guessGame.js. Still having trouble determining how to generate an output message based on user inputs rather than a hard-coded input.

--- guessGame.js
+++ guessGame.js
@@ -1,1 +1,20 @@
+var userGuess = parseInt(document.getElementsByName("userGuess"), 10);
+
+var answer = Math.round(Math.random() * 100);
+
+var game = function() {
+	
+	if (answer > 50) {
+		console.log("That's high!");
+	}
+	else {
+		console.log("That's low!");
+	}
+	
+}
+
+console.log(answer);
+
+console.log(game());
+
 console.log("Hello World!")
