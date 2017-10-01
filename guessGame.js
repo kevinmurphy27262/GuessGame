b20ead6e41e14b8c4dbf883ac4936084548e7c12
Kevin Murphy <kevin.murphy@hubinternational.com>
2017-09-30 23:18:21 -0500

    This is a very transitional period in the project in which I have begun to introduce guess dynamics without establishing boundaries or any other advanced mechanics. Lots of extra, uneeded code exists as I am working through a few idea experiments.

--- guessGame.js
+++ guessGame.js
@@ -1,20 +1,51 @@
-var userGuess = parseInt(document.getElementsByName("userGuess"), 10);
-
 var answer = Math.round(Math.random() * 100);
 
 var game = function() {
 	
-	if (answer > 50) {
-		console.log("That's high!");
-	}
-	else {
-		console.log("That's low!");
+	var userGuess = parseInt(document.getElementsByName("userGuess")[0].value);
+	
+	var guessMax = 100;
+	var guessMin = 1;
+	
+	function {
+		
+		if (userGuess < guessMin) {
+			alert("Guess again, low");
+		}
+		else if {
+		userGuess > guessMax);
+		}
 	}
 	
+	/*function() {
+	
+		if (answer > userGuess) {
+		alert("Nope! Too low!");
+		}
+		else if (userGuess == NaN) {
+		alert("Not a number, guess again!");
+		}
+		else if (answer < userGuess) {
+		alert("Nope! Too high!");
+		}
+		else {
+		alert("You Guessed it correctly! Genius!");
+		}
+	
+	}*/
+	
+}
+
+var limits = function() {
+	
+	if(answer > 100) {
+		alert("Your guess was too high, guess again!");
+	}
+	else if (answer <= 0) {
+		alert("Your guess was too low, guess again!");
+	}
 }
 
 console.log(answer);
 
-console.log(game());
-
-console.log("Hello World!")
+console.log("Hello World!");
